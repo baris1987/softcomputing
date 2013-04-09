88e33f1d999f20ec1b67a11ecf46d148be6371ab
Christopher Althaus <althaus.christopher@gmail.com>
2013-04-09 23:02:17 +0200

    Preprocessor angepasst
    Da das Programm auf dem Paypal Capture basiert, bei dem die Textfarbe
    und der Hintergrund jedes mal gleich war, wurde hier der Text einfach
    herrausgezogen, indem die richtige Graustufe gewählt wurde.
    
    Das geht bei uns aber nicht, da wir unsere Textfarbe zufällig wählen,
    ebenso wie die Striche die wir mit hinzugefügt haben.
    -> Ich habe jetzt in dem Bild die am häufigsten auftretende Farbe (die
    Hintergrundfarbe wird ignoriert) gesucht und anschließend nur noch diese
    als Text übernommen.
    Bei den ersten Versuchen hat das super geklappt. Ein Problem hat man nur
    wenn die Farbe der Striche mal die gleiche ist wie die des Textes.

--- MATLAB/src/preprocess.m
+++ MATLAB/src/preprocess.m
@@ -6,10 +6,31 @@ function bounded = preprocess(i)
 %
 % Created by Kurt Alfred Kluever (kurt@kloover.com)
 
-    % Convert the color image to grey scale
+
+  
+    % Bild in Graustufen umwandeln
     greyScale = rgb2gray(i);
-    % Threshold out the background noise
-    thresholded = greyScale < 75;
+    % Alle vorhandenen Farben auflisten
+    imageColors = unique(greyScale(:));
+    % Der Hintergrund interessiert nicht, deshalb wird die Farbe aus der
+    % Liste gelöscht
+    imageColors = imageColors(imageColors~=255);
+    % Zählen wie oft die einzellnen Farben vorkommen
+    colorCounts = histc(greyScale, imageColors);
+    sumRow = sum(colorCounts,2);
+    
+    % Das maximum sollte nun hoffentlich der Textfarbe entsprechen
+    [maxNum, maxInd] = max(sumRow);
+    txt_color = imageColors(maxInd);
+      
+    subplot(4,1,2);
+    imshow(greyScale);
+    title('After rgb2gray');
+    % Nur noch die Textfrabe wird angezeigt
+    thresholded = greyScale == txt_color;
+    subplot(4,1,3);
+    imshow(thresholded);
+    title('After cleaning');
     % Remove any random noise that will hurt the char offset
     rows = size(thresholded, 1);
     row = 1;
@@ -26,4 +47,7 @@ function bounded = preprocess(i)
 
     % Crop out the contents of the bounding box
     bounded = imcrop(thresholded, bb.BoundingBox);
+    subplot(4,1,4);
+    imshow(bounded);
+    title('After cutting');
 end
