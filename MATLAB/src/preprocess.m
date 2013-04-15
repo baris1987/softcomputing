1030f92c65db504e1c2ccffce83142060c991981
Christopher Althaus <althaus.christopher@gmail.com>
2013-04-15 10:59:53 +0200

    Preprocessor auf Grauwerte angepasst

--- MATLAB/src/preprocess.m
+++ MATLAB/src/preprocess.m
@@ -10,38 +10,17 @@ function bounded = preprocess(i)
   
     % Bild in Graustufen umwandeln
     greyScale = rgb2gray(i);
-    % Alle vorhandenen Farben auflisten
-    imageColors = unique(greyScale(:));
-    % Der Hintergrund interessiert nicht, deshalb wird die Farbe aus der
-    % Liste gelöscht
-    imageColors = imageColors(imageColors~=255);
-    % Zählen wie oft die einzellnen Farben vorkommen
-    colorCounts = histc(greyScale, imageColors);
-    sumRow = sum(colorCounts,2);
-    
-    % Das maximum sollte nun hoffentlich der Textfarbe entsprechen
-    [maxNum, maxInd] = max(sumRow);
-    txt_color = imageColors(maxInd);
-      
     subplot(4,1,2);
     imshow(greyScale);
     title('After rgb2gray');
     % Nur noch die Textfrabe wird angezeigt
-    thresholded = greyScale == txt_color;
+    thresholded = greyScale < 65;
     subplot(4,1,3);
     imshow(thresholded);
     title('After cleaning');
-    % Remove any random noise that will hurt the char offset
-    rows = size(thresholded, 1);
-    row = 1;
-    while (row < rows)
-        rowSum = sum(thresholded(row,:));
-        if (rowSum < 5 && rowSum > 0)
-           thresholded(row,:) = 0;
-        end
-        row = row + 1;
-    end
+
     
+  
     % Place a bounding box around the image
     bb = regionprops(double(thresholded), 'BoundingBox');
 
