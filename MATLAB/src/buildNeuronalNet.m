5344e08ddd68db5fdebf8f2752cb0636216b7d6d
majn <mtthsjntsch@gmail.com>
2013-04-15 22:44:53 +0300

    Update buildNeuronalNet.m
    Definieren der Anzahl der Ein- und Ausgabeknoten funktioniert automatisch. Code ist unnötig.

--- MATLAB/src/buildNeuronalNet.m
+++ MATLAB/src/buildNeuronalNet.m
@@ -2,22 +2,14 @@
     Erstellt und trainiert ein neuronales Netz zur Musterkennung, um
     Buchstaben auf Bildern zu erkennen. 
 
-    x: Anzahl der Pixel in x-Richtung
-    y: Anzahl der Pixel in y-Richtung
     in:  (x*y)x1 Cell-Arrays mit Eingabebildern. Schwarz = 1, Weiß = 0;
     out: 36x1 Cell-Arrays mit Zielwerten.
 %}
     
-function net = buildNeuronalnet(x,y,in,out)
+function net = buildNeuronalnet(in,out)
 
     net = patternnet(10);
 
-    % Jedes Pixel ist ein Eingabewert ins neuronale Netz.
-    net.numInputs = x*y;
-    
-    % 10 Zahlen und 26 Buchstaben
-    net.numOutputs = 36;
-    
     % Netzwerk trainieren.
-    train(net,in,out);
+    net = train(net,in,out);
 end
