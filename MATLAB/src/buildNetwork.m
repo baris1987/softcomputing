e4fac7e20a616933eb950eaaa99b73794bb972a0
baris2887 <AkdagBa45587@ohm-hochschule.de>
2013-05-06 08:45:55 +0200

    Refactoring.

--- MATLAB/src/buildNetwork.m
+++ MATLAB/src/buildNetwork.m
@@ -1,18 +1,21 @@
-function makeTemplates()
-% Creates the classification templates using a set of training images. The
-% training images are labeled as ground truth by their filenames.
-%
-% Created by Kurt Alfred Kluever (kurt@kloover.com)
-
-    % Set aside room for the templates and the counts
-    templates = zeros(20,20,36);
-    counts = zeros(1,36);
+function chrMakeTemplates()
 
     % Load the images from the training directory
-    trainingDir = 'training/';    
+    trainingDir = 'D:\Uni\softcomputing\MATLAB\images/Training/';    
     trainingSamples = dir(strcat(trainingDir, '*.png'));
     [numTrainingSamples] = size(trainingSamples);
-    net = patternnet(10);
+ 	trainingSamplesSize = numTrainingSamples(1,2) * 6;
+    in = zeros(60,trainingSamplesSize);
+    out = zeros(36,trainingSamplesSize);
+    % Create a Pattern Recognition Network
+    hiddenLayerSize = 40;
+    net = patternnet(hiddenLayerSize);
+
+    % Setup Division of Data for Training, Validation, Testing
+    net.divideParam.trainRatio = 70/100;
+    net.divideParam.valRatio = 15/100;
+    net.divideParam.testRatio = 15/100;
+
     % For each of the training images...
     for i=1:numTrainingSamples
         filename = strcat(trainingDir, trainingSamples(i).name);  
@@ -27,9 +30,9 @@ function makeTemplates()
         filename = strrep(filename, trainingDir, '');
         
         % For each of the characters...
-        for i=1:5
+        for j=1:6
             % Convert from ASCII character to ASCII int (an index)
-            asciiz = uint8(filename(i));
+            asciiz = uint8(filename(j));
             if (asciiz >= 48 && asciiz <= 57) % number
                 asciiz = asciiz - 47;
             elseif (asciiz >= 65 && asciiz <=90) % upper case character
@@ -37,22 +40,27 @@ function makeTemplates()
             else
                 % We should never, ever get here!
             end
-            
-            net = train(net,chars(:,:,i),asciiz);
-            % Add the template to the training library
-            %templates(:,:,asciiz) = templates(:,:,asciiz) + chars(:,:,i);
 
+            inVP = sum(chars(:,:,j),2);
+            inHP = sum(chars(:,:,j)',2);
+            in(:,(i-1)*6+j) = [inVP;inHP];
+            out(asciiz,(i-1)*6+j) = 1;
+            
             % Update the number of times we have seen this character
             %counts(asciiz) = counts(asciiz) + 1;
         end
     end
-    
-    % Go through and normalize (average) the templates
-    %for i=1:36
-    %    templates(:,:,i) = templates(:,:,i) / counts(i);
-    %end
-    
+   % Train the Network
+    [net,tr] = train(net,in,out);
+
+    % Test the Network
+    outputs = net(in);
+    errors = gsubtract(out,outputs);
+    performance = perform(net,out,outputs)
+
+    % View the Network
+    view(net)
+
     % Save them out to the file for later
-    %save('templates.mat', 'templates', 'counts');
-    save('neuronal.mat', 'net');
+    save('neuronal.mat', 'net', 'in', 'out');
 end
