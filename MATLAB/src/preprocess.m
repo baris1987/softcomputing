e075ad6f2c7e7439bafe6b7cc4b38d894f7d93ef
Christopher Althaus <althaus.christopher@gmail.com>
2013-06-24 12:54:35 +0200

    lstlisting für Code input hinzugefügt / Doku angefangen

--- MATLAB/src/preprocess.m
+++ MATLAB/src/preprocess.m
@@ -1,25 +1,6 @@
-function bounded = preprocess(i)
-% Performs preprocessing on the input image.
-  
-    % Bild in Graustufen umwandeln
-    greyScale = rgb2gray(i);
-  %  subplot(4,1,2);
-   % imshow(greyScale);
-   % title('After rgb2gray');
-    % Nur noch die Textfrabe wird angezeigt
-    thresholded = greyScale < 65;
-    %subplot(4,1,3);
-   % imshow(thresholded);
-   % title('After cleaning');
+function cleaned = preprocess(image)
 
-    
-  
-    % Place a bounding box around the image
-    bb = regionprops(double(thresholded), 'BoundingBox');
+greyScale = rgb2gray(image);
+cleaned = greyScale < 65;
 
-    % Crop out the contents of the bounding box
-    bounded = imcrop(thresholded, bb.BoundingBox);
-    %subplot(4,1,4);
-    %imshow(bounded);
-   % title('After cutting');
 end
